f682600ad47ebb2ef29b847745448453d6791502
partha biswas <biswas.partha1@gmail.com>
2024-04-15 01:35:21 +0530

    using generic json serializer and deserializer

--- Ex02SerializationAndDeserialization/src/main/java/org/example/serdes/SerdesFactory.java
+++ Ex02SerializationAndDeserialization/src/main/java/org/example/serdes/SerdesFactory.java
@@ -1,11 +1,18 @@
 package org.example.serdes;
 
 import org.apache.kafka.common.serialization.Serde;
+import org.apache.kafka.common.serialization.Serdes;
 import org.example.domain.GreetingMessage;
 
 public class SerdesFactory {
 
     public static Serde<GreetingMessage> greetingMessageSerde(){
         return new GreetingSerde();
+    }
+
+    public static Serde<GreetingMessage> greetingSerdesusingGenerics(){
+        JsonSerializer<GreetingMessage> jsonSerializer = new JsonSerializer<>();
+        JsonDeserializer<GreetingMessage> jsonDeserializer = new JsonDeserializer<>(GreetingMessage.class);
+        return Serdes.serdeFrom(jsonSerializer,jsonDeserializer);
     }
 }
