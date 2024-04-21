358fa726d1fdc8e17da74a7fc204cbcc9aeec56f
partha biswas <biswas.partha1@gmail.com>
2024-04-21 19:46:11 +0530

    transform orders to revenue and post to topic

---
+++ Ex03RetailApp3SplitAndPostOnlyOrderAmount/src/main/java/org/example/domain/Store.java
@@ -0,0 +1,6 @@
+package org.example.domain;
+
+public record Store(String locationId,
+                    Address address,
+                    String contactNum) {
+}
---
+++ Ex03RetailApp3SplitAndPostOnlyOrderAmount/src/main/java/org/example/topology/OrdersTopology.java
@@ -0,0 +1,66 @@
+package org.example.topology;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.Topology;
+import org.apache.kafka.streams.kstream.*;
+import org.example.domain.Order;
+import org.example.domain.OrderType;
+import org.example.domain.Revenue;
+import org.example.serdes.SerdesFactory;
+
+import static org.apache.kafka.streams.kstream.Printed.toSysOut;
+
+@Slf4j
+public class OrdersTopology {
+    public static final String ORDERS = "orders";
+
+    public static final String STORES = "stores";
+
+    public static final String RESTAURANT_ORDERS = "restaurant_orders";
+
+    public static final String GENERAL_ORDERS = "general_orders";
+
+    public static Topology buildTopology() {
+
+        Predicate<String, Order> generalPredicate = (key, order) -> order.orderType().equals(OrderType.GENERAL);
+
+        Predicate<String, Order> restaurantPredicate = (key, order) -> order.orderType().equals(OrderType.RESTAURANT);
+
+        ValueMapper<Order, Revenue> revenueMapper = order -> new Revenue(order.locationId(), order.finalAmount());
+
+        StreamsBuilder streamsBuilder = new StreamsBuilder();
+        var ordersStream = streamsBuilder.stream(ORDERS,
+                Consumed.with(Serdes.String(), SerdesFactory.orderSerde())
+        );
+
+        ordersStream.print(Printed.<String, Order>toSysOut().withLabel("orders"));
+
+        //thus here we are splitting one single stream based upon some predicate logic into multiple streams i.e. generalOrdersStram and restaurantOrderStream
+        ordersStream.split(Named.as("General-restaurant-stream")) //passing this parameter is optional
+                .branch(generalPredicate, Branched.withConsumer(generalOrdersStream -> {
+                    //any processing logic for general orders can be inserted here
+                    generalOrdersStream
+                            .print(Printed.<String, Order>toSysOut().withLabel("general stream")); //printing out just for debugging purposes
+
+                    generalOrdersStream
+                            .mapValues((readOnlyKey, value) -> revenueMapper.apply(value))
+                            .to(GENERAL_ORDERS, Produced.with(Serdes.String(), SerdesFactory.revenueSerde()));
+
+                }))
+                .branch(restaurantPredicate, Branched.withConsumer(restaurantOrderStreams -> {
+                    //any processing logic for restaurant orders can be inserted here
+                    restaurantOrderStreams.print(Printed.<String, Order>toSysOut().withLabel("restaurant stream"));//printing out just for debugging purposes
+
+                    restaurantOrderStreams
+                            //.to(RESTAURANT_ORDERS,Produced.with(Serdes.String(), SerdesFactory.orderSerde()));
+                            .mapValues((readOnlyKey, value) -> revenueMapper.apply(value))
+                            .to(RESTAURANT_ORDERS, Produced.with(Serdes.String(), SerdesFactory.revenueSerde()));
+
+                }));
+
+
+        return streamsBuilder.build();
+    }
+}
