6504626c664ebbebe7a20f40931f6d25c27bf106
partha biswas <biswas.partha1@gmail.com>
2024-04-26 00:58:10 +0530

    Ktable example

---
+++ Ex05Ktable/src/main/java/org/example/Ex01KTableApp.java
@@ -0,0 +1,61 @@
+package org.example;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.admin.AdminClient;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsConfig;
+import org.example.topology.Ex01KTableToplogy;
+
+import java.util.List;
+import java.util.Properties;
+import java.util.stream.Collectors;
+
+import static org.example.topology.Ex01KTableToplogy.WORDS;
+
+@Slf4j
+public class Ex01KTableApp {
+
+
+    public static void main(String[] args) {
+
+        var kTableTopology = Ex01KTableToplogy.build();
+
+        Properties config = new Properties();
+        config.put(StreamsConfig.APPLICATION_ID_CONFIG, "ktable"); // consumer group
+        config.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        config.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest");
+
+        createTopics(config, List.of(WORDS));
+        var kafkaStreams = new KafkaStreams(kTableTopology, config);
+
+        Runtime.getRuntime().addShutdownHook(new Thread(kafkaStreams::close));
+
+        log.info("Starting Greeting streams");
+        kafkaStreams.start();
+    }
+
+    private static void createTopics(Properties config, List<String> greetings) {
+
+        AdminClient admin = AdminClient.create(config);
+        var partitions = 1;
+        short replication  = 1;
+
+        var newTopics = greetings
+                .stream()
+                .map(topic ->{
+                    return new NewTopic(topic, partitions, replication);
+                })
+                .collect(Collectors.toList());
+
+        var createTopicResult = admin.createTopics(newTopics);
+        try {
+            createTopicResult
+                    .all().get();
+            log.info("topics are created successfully");
+        } catch (Exception e) {
+            log.error("Exception creating topics : {} ",e.getMessage(), e);
+        }
+    }
+}
---
+++ Ex05Ktable/src/main/java/org/example/topology/Ex01KTableToplogy.java
@@ -0,0 +1,33 @@
+package org.example.topology;
+
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.Topology;
+import org.apache.kafka.streams.kstream.Consumed;
+import org.apache.kafka.streams.kstream.Materialized;
+import org.apache.kafka.streams.kstream.Printed;
+
+public class Ex01KTableToplogy {
+
+    public static String WORDS = "words";
+
+
+    public static Topology build(){
+
+        StreamsBuilder streamsBuilder = new StreamsBuilder();
+
+        var wordsTable = streamsBuilder
+                .table(WORDS,
+                        Consumed.with(Serdes.String(), Serdes.String())
+                );
+
+        wordsTable
+                .filter((key, value) -> value.length() > 2)
+                .toStream()
+                .print(Printed.<String,String>toSysOut().withLabel("words-ktable"));
+
+
+        return streamsBuilder.build();
+
+    }
+}
