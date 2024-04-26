8a4d94cbe8f013901c8ae2b313e04406f9dcf359
partha biswas <biswas.partha1@gmail.com>
2024-04-26 16:49:45 +0530

    the count aggregation operator

---
+++ Ex06StatefulOperations/src/main/java/org/example/producer/AggregateProducer.java
@@ -0,0 +1,46 @@
+package org.example.producer;
+
+import lombok.extern.slf4j.Slf4j;
+
+
+import static org.example.producer.ProducerUtil.publishMessageSync;
+import static org.example.topology.Ex01AggregationCountOperatorTopology.AGGREGATE;
+
+@Slf4j
+public class AggregateProducer {
+
+
+    public static void main(String[] args) throws InterruptedException {
+
+        var key = "A";
+        //String key = null;
+
+        var word = "Apple";
+        var word1 = "Alligator";
+        var word2 = "Ambulance";
+
+        var recordMetaData = publishMessageSync(AGGREGATE, key,word);
+        log.info("Published the alphabet message : {} ", recordMetaData);
+
+        var recordMetaData1 = publishMessageSync(AGGREGATE, key,word1);
+        log.info("Published the alphabet message : {} ", recordMetaData1);
+
+        var recordMetaData2 = publishMessageSync(AGGREGATE, key,word2);
+        log.info("Published the alphabet message : {} ", recordMetaData2);
+
+        var bKey = "B";
+        //String bKey = null;
+
+        var bWord1 = "Bus";
+        var bWord2 = "Baby";
+        var recordMetaData3 = publishMessageSync(AGGREGATE, bKey,bWord1);
+        log.info("Published the alphabet message : {} ", recordMetaData2);
+
+        var recordMetaData4 = publishMessageSync(AGGREGATE, bKey,bWord2);
+        log.info("Published the alphabet message : {} ", recordMetaData2);
+
+    }
+
+
+
+}
---
+++ Ex06StatefulOperations/src/main/java/org/example/topology/Ex01AggregationCountOperatorTopology.java
@@ -0,0 +1,41 @@
+package org.example.topology;
+
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.Topology;
+import org.apache.kafka.streams.kstream.*;
+
+public class Ex01AggregationCountOperatorTopology {
+
+
+    public static String AGGREGATE = "aggregate";
+
+    public static Topology build(){
+        StreamsBuilder streamsBuilder = new StreamsBuilder();
+
+        var inputStream = streamsBuilder
+                .stream(AGGREGATE,
+                        Consumed.with(Serdes.String(),Serdes.String()));
+
+        inputStream
+                .print(Printed.<String,String>toSysOut().withLabel(AGGREGATE));
+
+        var groupedString = inputStream
+                .groupByKey(Grouped.with(Serdes.String(), Serdes.String()));
+
+        exploreCount(groupedString);
+
+        return streamsBuilder.build();
+    }
+
+    private static void exploreCount(KGroupedStream<String, String> groupedStream) {
+
+        var countByAlphabet = groupedStream
+                .count(Named.as("count-per-alphabet"));
+
+        countByAlphabet
+                .toStream()
+                .print(Printed.<String,Long>toSysOut().withLabel("words-count-per-alphabet"));
+
+    }
+}
